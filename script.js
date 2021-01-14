f46a065ba6b88d124a2c227e7ae8bd60053708ed
433MHz <krystian.izdebski1999@gmail.com>
2021-01-14 12:12:13 +0100

    Ships game  with additional comments in game

--- script.js
+++ script.js
@@ -19,12 +19,16 @@ while(isSunk == false){
         guesses = guesses + 1;
 
         if(guess == location1 || guess == location2 || guess == location3){
-            hits = hits + 1;
+            hits = hits + 1; 
+            alert("Trafionty!");
 
             if(hits == 3){
                 isSunk = true;
                 alert("Zatopiłeś mój okręt!");
             }
+        }
+        else{
+            alert("Pudło!");
         }
     }
 }
