daabdd618f703da602dc782417019561487e617a
433MHz <krystian.izdebski1999@gmail.com>
2021-01-14 12:08:23 +0100

    Ships game working version

--- script.js
+++ script.js
@@ -10,6 +10,24 @@ var isSunk = false;
 
 
 while(isSunk == false){
-
     guess = prompt("Gotów, cel, pal! (Podaj liczbę z zakresu od 0-6):");
+
+    if(guess < 0 || guess > 6){
+        alert("Proszę podać prawidłowy numer komórki!");
+    }
+    else{
+        guesses = guesses + 1;
+
+        if(guess == location1 || guess == location2 || guess == location3){
+            hits = hits + 1;
+
+            if(hits == 3){
+                isSunk = true;
+                alert("Zatopiłeś mój okręt!");
+            }
+        }
+    }
 }
+
+var stats = "Potrzebowałeś " + guesses + " prób, by zatopić okręt, " + "czyli Twoja efektywność wynosi: " + (3/guesses) + ".";
+alert(stats);
