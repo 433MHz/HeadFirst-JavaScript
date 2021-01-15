7c551ac4cd6c004c2fa56596277a02627be45ab5
433MHz <krystian.izdebski1999@gmail.com>
2021-01-15 11:27:45 +0100

    tablice

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var zmiennaGlobalna = 1;
+function makePhrases(){
+    var words1 = ["nieprzerwane", "wielowarstwowe","tysiącmetrowe","biznesowe","niepowstrzymane"];
+    var words2 = ["wspomagane","wartościowe","zorientowane","skoncentrowane","wyrównane"];
+    var words3 = ["procesy","rozwiązania","punkty","strategie","wizje"];
 
+    var rand1 = Math.floor(Math.random() * words1.length);
+    var rand2 = Math.floor(Math.random() * words2.length);
+    var rand3 = Math.floor(Math.random() * words3.length);
 
-function funkcja(parametr1, parametr2){
-    var zmiennaLokalna;
-    zmiennaLokalna = parametr1 + parametr2;
-    return zmiennaLokalna;
+    var phrase = words1[rand1] + " " + words2[rand2] + " " + words3[rand3];
+    alert(phrase);
 }
 
-argument1 = 5;
-argument2 = 10;
-
-zmiennaGlobalna = funkcja(argument1, argument2);
-console.log(zmiennaGlobalna);
+makePhrases();
