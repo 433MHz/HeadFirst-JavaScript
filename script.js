a799654b7bf3cba584c86e05d37b084262496d97
433MHz <krystian.izdebski1999@gmail.com>
2021-01-16 12:11:20 +0100

    Refaktoryzacja kodu
    Dodanie funkcji

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 
 
 var scores = [60, 50, 60, 58,54,54,58,50,52,54,48,69,34,55,51,52,44,51,69,64,66,55,52,61,46,31,57,52,44,18,41,53,55,61,51,44];
-var output;
-var highScore = 0;
-var bestSolutions = [];
 
-for(var i = 0; i < scores.length; i++){
-    output = "Płyn do baniek numer: " + i + " wynik: " + scores[i];
-    console.log(output);
+function printAndGetHighScores(scores){
+    var output;
+    var highScore = 0;
+    for(var i = 0; i < scores.length; i++){
+        output = "Płyn do baniek numer: " + i + " wynik: " + scores[i];
+        console.log(output);
 
-    if(scores[i] > highScore){
-        highScore = scores[i];
+        if(scores[i] > highScore){
+            highScore = scores[i];
+        }
     }
+    return highScore;
 }
 
-for(var i = 0; i< scores.length; i++){
-    
-    if(highScore == scores[i]){
-        bestSolutions.push(scores[i]);
+function getBestResults(scores, highScore){
+    var bestSolutions = [];
+    for(var i = 0; i< scores.length; i++){
+
+        if(highScore == scores[i]){
+            bestSolutions.push(i);
+        }
     }
+    return bestSolutions;
 }
+
+var highScore = printAndGetHighScores(scores);
+var bestSolutions = getBestResults(scores, highScore);
 
 console.log("Liczba prób: " + scores.length);
 console.log("Największa ilość baniek: " + bestSolutions);
