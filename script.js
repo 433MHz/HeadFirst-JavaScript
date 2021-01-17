afeca190276ca28461fbbcd320096e4f97fa397e
433MHz <krystian.izdebski1999@gmail.com>
2021-01-17 19:11:45 +0100

    Pętla for in dla obiektu

--- script.js
+++ script.js
@@ -1,28 +1,91 @@
-var taxi = {
-    make: "SieMoCorp",
-    model: "taxi",
+var cadi = {
+    make: "GM",
+    model: "Cadillac",
     year: 1955,
-    color: "żółty",
-    passengers: 4,
+    color: "jasnobrązowy",
+    passengers: 5,
     convertible: false,
-    mileage: 281341,
-    isRunning: false,
-    turnOn: function(){
-        this.isRunning = true;
+    mileage: 12892,
+
+    started: false,
+
+    start: function(){
+        this.started = true;
     },
-    turnOff: function(){
-        this.isRunning = false;
+
+    stop: function(){
+        this.started = false;
     },
+
     drive: function(){
-        if(this.isRunning){
-            alert("Wrruuum");
+        if(this.started){
+            alert(this.make + " " + this.model + " Robi: brum wrrrr!");
         }
         else{
-            alert("First you need to turn on car");
+            alert("Najpierw musisz uruchomić silnik");
         }
     }
 };
 
+var chevy = {
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "czerwony",
+    passengers: 2,
+    convertible: false,
+    mileage: 1021,
 
-taxi.turnOn();
-taxi.drive();
+    started: false,
+
+    start: function(){
+        this.started = true;
+    },
+
+    stop: function(){
+        this.started = false;
+    },
+
+    drive: function(){
+        if(this.started){
+            alert(this.make + " " + this.model + " Robi: brum wrrrrr!");
+        }
+        else{
+            alert("Najpier musisz włączyć silnik");
+        }
+    }
+};
+
+var taxi = {
+    make: "SieMoCorp",
+    model: "Taxi",
+    year: 1955,
+    color: "yellow",
+    passengers: 4,
+    convertible: false,
+    mileage: 281341,
+
+    started: false,
+
+    start: function(){
+        this.started = true;
+    },
+
+    stop: function(){
+        this.started = false;
+    },
+
+    drive: function(){
+        if(this.started){
+            alert(this.make + " " + this.model + " robi: brum wrrrr!");
+        }
+        else{
+            alert("Najpierw musisz włączyć silnik!");
+        }
+    }
+};
+
+for(var properties in chevy){
+    console.log(chevy[properties]);
+}
+
