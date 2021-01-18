e0aa803d5258a1d34710b314c7cc5ea9aa62ca28
433MHz <krystian.izdebski1999@gmail.com>
2021-01-18 18:17:36 +0100

    Typy danych w zmiennych

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+var test1 = "abcdef";
+var test2 = 123;
+var test3 = true;
+var test4 = {};
+var test5 = [];
+var test6;
+var test7 = {"abcdef": 123};
+var test8 = ["abcdef", 123];
+function test9() {return "abcdef"};
 
-
-function change(){
-var redplanet = document.getElementById("redplanet");
-redplanet.innerHTML = "Jest niedobrzeeee";
-redplanet.setAttribute("class", "redtext");
-}
-
-
-window.onload = change;
+console.log(typeof test1);
+console.log(typeof test2);
+console.log(typeof test3);
+console.log(typeof test4);
+console.log(typeof test5);
+console.log(typeof test6);
+console.log(typeof test7);
+console.log(typeof test8);
+console.log(typeof test9);
